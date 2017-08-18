1a1303fa88b20df8276f2f2e4559e75f8edb59e7
Tejasvi Nareddy <tejunareddy@gmail.com>
2017-08-18 00:17:18 -0400

    More progress

--- get_post_by_id.py
+++ get_post_by_id.py
@@ -9,11 +9,17 @@ with open(inFile) as data_file:
 
 linksById = {}
 
+count = 0
 for comment in data:
+
+    if count > 2:
+        break
 
     postId = comment['link_id']
 
     if postId not in linksById:
+
+        count += 1
 
         url = 'https://www.reddit.com/by_id/' + postId + '.json'
         headers = {'user-agent': 'teju-reddit-api-accessing' + str(randint(0, 1000))}
@@ -21,10 +27,13 @@ for comment in data:
 
         if r.status_code == 200:
             print(postId)
-            linksById[postId] = r.json()
+            linksById[postId] = r.json()['data']['children'][0]['data']
         else:
             print("Failure for comment ", comment)
 
 outFile = "./data/parent_posts_july.json"
-with open(outFile, 'w') as outfile:
-    json.dump(linksById, outfile)
+with open(outFile, 'w') as f:
+    f.write('[')
+    myList = ','.join(map(str, linksById.values()))
+    json.dump(myList, f)
+    f.write(']')
